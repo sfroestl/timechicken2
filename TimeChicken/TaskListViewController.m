e580f7e111610b37c2ad9174c7d9f062cb7ba243
christian <cs85@me.com>
2013-01-15 13:31:36 +0100

    adds reusable TableViewCells

--- TimeChicken/TaskListViewController.m
+++ TimeChicken/TaskListViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "TaskListViewController.h"
+#import "TCTaskStore.h"
+#import "TCTask.h"
 
 @interface TaskListViewController ()
 
@@ -19,6 +21,9 @@
     self = [super initWithStyle:UITableViewStyleGrouped];
     if(self){
         self.title = @"Tasks";
+        for (int i=0; i<5; i++){
+            [[TCTaskStore taskStore] createNewTask];
+        }
     }
     return self;
 }
@@ -48,5 +53,25 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [[[TCTaskStore taskStore] openTasks] count];
+}
+
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    //check for reusable cell first, use that if it exists
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"];
+    //If there is no reusable cell of this type, create a new one
+    if(!cell){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"UITableViewCell"];
+    }
+    //Set the the text on the cell with the title of the task
+    //that is at the nth index of items, where n = row this cell will appear in the tableView
+    TCTask * task = [[[TCTaskStore taskStore] openTasks] objectAtIndex:[indexPath row]];
+    [[cell textLabel] setText:[task title]];
+    return cell;
+}
+
+
 
 @end
