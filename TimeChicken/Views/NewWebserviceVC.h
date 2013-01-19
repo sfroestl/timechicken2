9f33aeaacb38b12f2cf28e36ce6925d0b0e0748e
Sebastian Fröstl <sebastian@froestl.com>
2013-01-19 12:37:09 +0100

    crazy webservice

---
+++ TimeChicken/Views/NewWebserviceVC.h
@@ -0,0 +1,13 @@
+//
+//  NewWebserviceVC.h
+//  TimeChicken
+//
+//  Created by Sebastian Fröstl on 18.01.13.
+//  Copyright (c) 2013 Christian Schäfer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewWebserviceVC : UIViewController
+
+@end
---
+++ TimeChicken/Views/Webservice/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TimeChicken
+//
+//  Created by Sebastian Fröstl on 18.01.13.
+//  Copyright (c) 2013 Christian Schäfer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
